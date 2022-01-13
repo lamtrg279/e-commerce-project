3c90742debf28ea12dfff3d54a18d457080496e3
Lam <nl2791999@gmail.com>
2022-01-12 20:42:19 -0600

    adding navbar and chec/commerce

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,36 +1,47 @@
 /** @format */
 
 import React from "react";
-// prettier-ignore
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from "@material-ui/core";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
 
-import UseStyles from "./styles";
+import useStyles from "./styles";
 
-const Product = ({ product }) => {
-  const classes = UseStyles();
+const Product = ({ product, onAddToCart }) => {
+  const classes = useStyles();
 
   return (
     <Card className={classes.root}>
       <CardMedia
         className={classes.media}
-        image={product.image}
+        image={product.image.url}
         title={product.name}
       />
       <CardContent>
         <div className={classes.cardContent}>
-          <Typography variant='h5' gutterBottom>
+          <Typography gutterBottom variant='h5' component='h2'>
             {product.name}
           </Typography>
-          <Typography variant='h5'>{product.price}</Typography>
+          <Typography gutterBottom variant='h5' component='h2'>
+            ${product.price.formatted}
+          </Typography>
         </div>
-        <Typography variant='body2' color='textSecondary'>
-          {" "}
-          {product.description}{" "}
-        </Typography>
+        <Typography
+          dangerouslySetInnerHTML={{ __html: product.description }}
+          variant='body2'
+          color='textSecondary'
+        />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label='Add to Cart'>
+        <IconButton
+          aria-label='Add to Cart'
+          onClick={() => onAddToCart(product.id, 1)}>
           <AddShoppingCart />
         </IconButton>
       </CardActions>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,30 +1,12 @@
 /** @format */
 
 import React from "react";
-import { Grid } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 
 import Product from "./Product/Product";
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Shoes",
-    description: "Running shoes",
-    price: "$50",
-    image:
-      "https://cdn.shopify.com/s/files/1/0419/1525/products/1024x1024-Men-Captain-Tobacco-1_800x800.jpg?v=1599662975",
-  },
-  {
-    id: 2,
-    name: "MacBook",
-    description: "Apple Macbook",
-    price: "$500",
-    image: "https://dlcdnrog.asus.com/rog/media/1546863952392.jpg",
-  },
-];
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
 
   return (
@@ -32,8 +14,8 @@ const Products = () => {
       <div className={classes.toolbar} />
       <Grid container justify='center' spacing={4}>
         {products.map((product) => (
-          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
