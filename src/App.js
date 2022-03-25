71352c353d3e3e04b2bb03d513702098f9796ee5
Lam <nl2791999@gmail.com>
2022-03-25 17:15:28 -0500

    review and payment frontend

--- src/App.js
+++ src/App.js
@@ -6,75 +6,108 @@ import { Products, Navbar, Cart, Checkout } from "./components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState({});
+	const [products, setProducts] = useState([]);
+	const [cart, setCart] = useState({});
+	const [order, setOrder] = useState({});
+	const [errorMessage, setErrorMessage] = useState("");
 
-  const fetchProducts = async () => {
-    const { data } = await commerce.products.list();
-    setProducts(data);
-  };
+	const fetchProducts = async () => {
+		const { data } = await commerce.products.list();
+		setProducts(data);
+	};
 
-  const fetchCart = async () => {
-    const cart = await commerce.cart.retrieve();
-    setCart(cart);
-  };
+	const fetchCart = async () => {
+		const cart = await commerce.cart.retrieve();
+		setCart(cart);
+	};
 
-  const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart);
-  };
+	const handleAddToCart = async (productId, quantity) => {
+		const item = await commerce.cart.add(productId, quantity);
+		setCart(item.cart);
+	};
 
-  const handleUpdateCartQty = async (productId, quantity) => {
-    const item = await commerce.cart.update(productId, { quantity });
-    setCart(item.cart);
-  };
+	const handleUpdateCartQty = async (productId, quantity) => {
+		const item = await commerce.cart.update(productId, { quantity });
+		setCart(item.cart);
+	};
 
-  const handleRemoveFromCart = async (productId) => {
-    const item = await commerce.cart.remove(productId);
-    setCart(item.cart);
-  };
+	const handleRemoveFromCart = async (productId) => {
+		const item = await commerce.cart.remove(productId);
+		setCart(item.cart);
+	};
 
-  const handleEmptyCart = async () => {
-    const item = await commerce.cart.empty();
-    setCart(item.cart);
-  };
+	const handleEmptyCart = async () => {
+		const item = await commerce.cart.empty();
+		setCart(item.cart);
+	};
 
-  useEffect(() => {
-    fetchProducts();
-    fetchCart();
-  }, []);
+	const refreshCart = async () => {
+		const newCart = await commerce.cart.refresh();
+		setCart(newCart);
+	};
 
-  console.log(products);
-  console.log(cart);
+	const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+		try {
+			const incomingOrder = await commerce.checkout.capture(
+				checkoutTokenId,
+				newOrder
+			);
+			setOrder(incomingOrder);
+			refreshCart();
+		} catch (error) {
+			setErrorMessage(error.data.error.message);
+		}
+	};
 
-  return (
-    <Router>
-      <div>
-        <Navbar totalItems={cart.total_items} />
-        <Routes>
-          <Route
-            exact
-            path='/'
-            element={
-              <Products products={products} onAddToCart={handleAddToCart} />
-            }
-          />
-          <Route
-            path='/cart'
-            element={
-              <Cart
-                cart={cart}
-                handleUpdateCartQty={handleUpdateCartQty}
-                handleRemoveFromCart={handleRemoveFromCart}
-                handleEmptyCart={handleEmptyCart}
-              />
-            }
-          />
-          <Route path='/checkout' element={<Checkout cart={cart} />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+	useEffect(() => {
+		fetchProducts();
+		fetchCart();
+	}, []);
+
+	console.log(products);
+	console.log(cart);
+
+	return (
+		<Router>
+			<div>
+				<Navbar totalItems={cart.total_items} />
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={
+							<Products
+								products={products}
+								onAddToCart={handleAddToCart}
+							/>
+						}
+					/>
+					<Route
+						path="/cart"
+						element={
+							<Cart
+								cart={cart}
+								handleUpdateCartQty={handleUpdateCartQty}
+								handleRemoveFromCart={handleRemoveFromCart}
+								handleEmptyCart={handleEmptyCart}
+							/>
+						}
+					/>
+					<Route
+						path="/checkout"
+						element={
+							<Checkout
+								cart={cart}
+								order={order}
+								onCaptureCheckout={handleCaptureCheckout}
+								error={errorMessage}
+							/>
+						}
+					/>
+				</Routes>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,41 +1,36 @@
-/** @format */
-
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { InputLabel, Select, MenuItem, Button, Grid, Typography } from "@material-ui/core";
-import { useForm, FormProvider } from "react-hook-form";
-
-import { commerce } from "../../lib/commerce";
-import FormInput from "./CustomTextField";
+import React, { useState, useEffect } from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { commerce } from '../../lib/commerce';
+import FormInput from './CustomTextField';
 
 const AddressForm = ({ checkoutToken, next }) => {
-    const methods = useForm();
     const [shippingCountries, setShippingCountries] = useState([]);
-    const [shippingCountry, setShippingCountry] = useState("");
+    const [shippingCountry, setShippingCountry] = useState('');
     const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
-    const [shippingSubdivision, setShippingSubdivision] = useState("");
+    const [shippingSubdivision, setShippingSubdivision] = useState('');
     const [shippingOptions, setShippingOptions] = useState([]);
-    const [shippingOption, setShippingOption] = useState("");
-
-    const countries = Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name }));
-    const subdivisions = Object.entries(shippingSubdivisions).map(([code, name]) => ({ id: code, label: name }));
-    const options = shippingOptions.map((sO) => ({ id: sO.id, label: `${sO.description} - (${sO.price.formatted_with_symbol})` }))
+    const [shippingOption, setShippingOption] = useState('');
+    const methods = useForm();
 
     const fetchShippingCountries = async (checkoutTokenId) => {
         const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
-        console.log(countries);
+
         setShippingCountries(countries);
         setShippingCountry(Object.keys(countries)[0]);
     };
 
     const fetchSubdivisions = async (countryCode) => {
         const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode);
+
         setShippingSubdivisions(subdivisions);
         setShippingSubdivision(Object.keys(subdivisions)[0]);
     };
 
-    const fetchShippingOptions = async (checkoutTokenId, country, region = null) => {
-        const options = await commerce.checkout.getShippingOptions(checkoutTokenId, { country, region });
+    const fetchShippingOptions = async (checkoutTokenId, country, stateProvince = null) => {
+        const options = await commerce.checkout.getShippingOptions(checkoutTokenId, { country, region: stateProvince });
+
         setShippingOptions(options);
         setShippingOption(options[0].id);
     };
@@ -54,37 +49,32 @@ const AddressForm = ({ checkoutToken, next }) => {
 
     return (
         <>
-            <Typography variant='h6' gutterBottom>
-                Shipping Address
-            </Typography>
+            <Typography variant="h6" gutterBottom>Shipping address</Typography>
             <FormProvider {...methods}>
                 <form onSubmit={methods.handleSubmit((data) => next({ ...data, shippingCountry, shippingSubdivision, shippingOption }))}>
                     <Grid container spacing={3}>
-                        <FormInput name='firstName' label='First Name' />
-                        <FormInput name='lastName' label='Last Name' />
-                        <FormInput name='address1' label='Address' />
-                        <FormInput name='email' label='Email' />
-                        <FormInput name='ZIP' label='Postal Code' />
-                        <FormInput name='City' label='City' />
+                        <FormInput required name="firstName" label="First name" />
+                        <FormInput required name="lastName" label="Last name" />
+                        <FormInput required name="address1" label="Address line 1" />
+                        <FormInput required name="email" label="Email" />
+                        <FormInput required name="city" label="City" />
+                        <FormInput required name="zip" label="Zip / Postal code" />
                         <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Country</InputLabel>
-                            <Select
-                                value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
-                                {countries.map((country) => (
-                                    <MenuItem key={country.id} value={country.id}>
-                                        {country.label}
+                            <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                                {Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name })).map((item) => (
+                                    <MenuItem key={item.id} value={item.id}>
+                                        {item.label}
                                     </MenuItem>
                                 ))}
                             </Select>
                         </Grid>
                         <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Subdivision</InputLabel>
-                            <Select
-                                value={shippingSubdivision}
-                                fullWidth onChange={(e) => setShippingSubdivision(e.target.value)}>
-                                {subdivisions.map((subdivision) => (
-                                    <MenuItem key={subdivision.id} value={subdivision.id}>
-                                        {subdivision.label}
+                            <Select value={shippingSubdivision} fullWidth onChange={(e) => setShippingSubdivision(e.target.value)}>
+                                {Object.entries(shippingSubdivisions).map(([code, name]) => ({ id: code, label: name })).map((item) => (
+                                    <MenuItem key={item.id} value={item.id}>
+                                        {item.label}
                                     </MenuItem>
                                 ))}
                             </Select>
@@ -92,9 +82,9 @@ const AddressForm = ({ checkoutToken, next }) => {
                         <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Options</InputLabel>
                             <Select value={shippingOption} fullWidth onChange={(e) => setShippingOption(e.target.value)}>
-                                {options.map((option) => (
-                                    <MenuItem key={option.id} value={option.id}>
-                                        {option.label}
+                                {shippingOptions.map((sO) => ({ id: sO.id, label: `${sO.description} - (${sO.price.formatted_with_symbol})` })).map((item) => (
+                                    <MenuItem key={item.id} value={item.id}>
+                                        {item.label}
                                     </MenuItem>
                                 ))}
                             </Select>
@@ -102,7 +92,7 @@ const AddressForm = ({ checkoutToken, next }) => {
                     </Grid>
                     <br />
                     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Button component={Link} to="/cart" variant="outlined">Back to Cart</Button>
+                        <Button component={Link} variant="outlined" to="/cart">Back to Cart</Button>
                         <Button type="submit" variant="contained" color="primary">Next</Button>
                     </div>
                 </form>
